2c44bc0b88c73afb93179e2c01309d83fecf0240
Sena Nur Yeşilyurt <61209847+senanuryesilyurt@users.noreply.github.com>
2021-10-27 20:35:26 +0300

    Add files via upload
    dictionary example

---
+++ MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic
+{
+    class MyDictionary<key,value>
+    {
+        key[] items_key;
+        value[] items_value;
+        //constructor
+        public MyDictionary()
+        {
+            items_key = new key[0];
+            items_value = new value[0];
+        }
+        
+        public void Add(key item1, value item2)
+        {
+            key[] tempArrayKeys = items_key;
+            value[] tempArrayValues = items_value;
+
+            items_key = new key[items_key.Length + 1];
+            items_value = new value[items_value.Length + 1];
+
+            for (int i = 0; i < tempArrayKeys.Length; i++)
+            {
+                items_key[i]= tempArrayKeys[i];
+            }
+            for (int j = 0; j < tempArrayValues.Length; j++)
+            {
+                items_value[j] = tempArrayValues[j];
+            }
+
+            items_key[items_key.Length - 1] = item1;
+            items_value[items_value.Length - 1] = item2;
+
+        }
+        public key[] fonk1
+        {
+            get { return items_key; }
+
+        }
+        public value[] fonk2
+        {
+            get { return items_value; }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> sairler = new MyDictionary<int, string>();
+            sairler.Add(1852, "Abdülhak Hamit Tahran");
+            sairler.Add(1927, "Ahmed Arif");
+            sairler.Add(1916, "Behçet Necatigil");
+            sairler.Add(1941, "Metin Altıok");
+            sairler.Add(1939, "Erdem Bayazıt");
+
+
+            Console.WriteLine("Doğum Yılı: " + sairler.fonk1[0] + " Adı: " + sairler.fonk2[0]);
+            Console.WriteLine("Doğum Yılı: " + sairler.fonk1[1] + " Adı: " + sairler.fonk2[1]);
+            Console.WriteLine("Doğum Yılı: " + sairler.fonk1[2] + " Adı: " + sairler.fonk2[2]);
+            Console.WriteLine("Doğum Yılı: " + sairler.fonk1[3] + " Adı: " + sairler.fonk2[3]);
+            Console.WriteLine("Doğum Yılı: " + sairler.fonk1[4] + " Adı: " + sairler.fonk2[4]);
+        }
+    }
+}
